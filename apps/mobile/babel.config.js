830dc060ae81fc0383f310e8050a8ca8d0e4b9ab
hakimasyrofi <hakimasyrofi@gmail.com>
2026-04-23 19:28:19 +0700

    update jsx import source

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -5,7 +5,6 @@ module.exports = function (api) {
       [
         "babel-preset-expo",
         {
-          jsxImportSource: "nativewind",
           unstable_transformImportMeta: true,
         },
       ],
